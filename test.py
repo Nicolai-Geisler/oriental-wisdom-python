ea767439897c9b3af0f5e321f2338bfe8201625d
Anonym-Player <nicolai.geisler11@gmail.com>
2024-05-28 11:29:50 +0200

    Add unsplash API support

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 # Import Flask webserver
+import urllib.request
 from flask import Flask, request, send_file, json, make_response
 # Import utilities
 import random
 import time
 import os
 import json
+import urllib
 # Importing the PIL library for image manipulation
 from PIL import Image, ImageDraw, ImageFont
 import textwrap
@@ -21,21 +23,33 @@ def getAllQuotes():
     return data.get('quotes', [])
 
 def getRandomImage():
+
+    # Use fallback if no unsplash images are available
+    directory = './images/unsplash/'
+    if os.listdir('./images/unsplash/') == []:
+        directory = './images/'
+
     # Get all images from folder
     pictures = []
-    for pic in os.listdir('./images/'):
+    for pic in os.listdir(directory):
         if pic.endswith(".jpg"):
             pictures.append(pic)
 
     # Select a random image as response
     picture = random.choice(pictures)
     global currentImage # Overwrite global variable
+
+    # make shure the same image is not beeing send twice in a row
+    while currentImage == picture:
+        picture = random.choice(pictures)
+    
     currentImage = picture
-    return 'images/' + picture
+
+    return directory + picture
 
 def createSharepic(dark, fancy):
     # Open an Image
-    img = Image.open("images/" + currentImage)
+    img = Image.open(currentImage)
     
     # Get quote and author
     quote = currentQuote["quote"]
@@ -43,10 +57,10 @@ def createSharepic(dark, fancy):
 
     # Set font and size
     font_path = "./fonts/Roboto-Regular.ttf"
-    font_size = 70
+    font_size = 80
     if str(fancy) == "true":
         font_path = "./fonts/Fancy.ttf"
-        font_size = 75
+        font_size = 85
     
     font_color = 255
     if str(dark) == "false":
@@ -88,6 +102,43 @@ def createSharepic(dark, fancy):
 
     # return the file
     return 'images/output.png'
+
+def getUnsplashImages():
+    
+    print("Getting Unsplash images...")
+
+    # URL parameters for unsplash API
+    baseUrl = 'https://api.unsplash.com/photos/random'
+    clientID = 'Ixp2xDIRS_6ocXQM0FMTii6OTjAPoCdyBRMUbp4MUxI'
+    count = 1
+    queries = ['nature', 'zen', 'landscape', 'asia']
+
+    # Download 10 images
+    i = 1
+    while i <= 10:
+        response = urllib.request.urlopen(baseUrl + '?client_id=' + clientID + '&count=' + str(count) + '&orientation=squarish&query=' + random.choice(queries))
+
+        response_body = response.read()
+
+        data = json.loads(response_body.decode("utf-8"))
+
+        imageUrls = ""
+        imageUrl = ""
+        imageName = "image"
+
+        for image in data:
+            imageName = image['id']
+            imageUrls = image['urls']
+            imageUrl = imageUrls['regular']
+
+            imageName = "images/unsplash/" + imageName + ".jpg"
+            urllib.request.urlretrieve(imageUrl, imageName)
+
+        i += 1
+    
+    return True
+
+# API endpoints
 
 @app.route('/api/get-quote', methods=['GET'])
 def send_quote():
@@ -161,7 +212,11 @@ def upload_image():
     except Exception as e:
         return str(e), 500
 
+# fetch images
+getUnsplashImages()
+
 # Global variables
+currentImage = ""
 currentImage = getRandomImage()
 quotes_list = getAllQuotes()
 currentQuote = random.choice(quotes_list)
